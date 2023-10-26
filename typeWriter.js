bb2515160a997499eac50d5b8a81618aa9174aaa
ChessZra <johnezrasee100@gmail.com>
2023-10-26 15:49:23 -0500

    test

--- typeWriter.js
+++ typeWriter.js
@@ -58,5 +58,5 @@ function displayText() {
 }
 
 let iteration = -1;
-const text = ["div.ChessZra {", "front-end-developer = False;", "True;", "}"];
+const text = ["div.eZra {", "front-end-dev: False;", "True;", "}"];
 displayText();
